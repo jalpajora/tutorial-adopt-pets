f70c4018d284fe7032e3f0c39fc45330911827bc
Jeza Mae Alpajora <jezamaealpajora@Jezas-MacBook-Pro-4.local>
2020-03-01 16:47:53 +0800

    feat: complete intro to react v5

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link, Redirect } from "@reach/router";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("ErrorBoundary caught an error", error, errorInfo);
+  }
+
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error with this listing. <Link to="/">Click here</Link>{" "}
+          to go back to the home page.
+        </h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
